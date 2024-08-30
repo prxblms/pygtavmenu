6368f545a7648dbe121339b74a502e252a128186
27prxblms <malcondev.lds@hotmail.com>
2024-08-30 18:49:31 -0300

    new

--- main.py
+++ main.py
@@ -1,38 +1,22 @@
 from src.functions import *
 
-class SafeZoneFrame(customtkinter.CTkFrame):
-    def __int__(self, master):
-        width = 100000
-        height = 100
-        super().__init__(master, width = width, height = height, fg_color = '#292929', corner_radius = 1)
-
-    # def DrawBackground(self):
-    #     backgroundColor = '#292929'
-
-    #     DrawBackground = DrawRect(self, width = 0.2250, height = 0.035, fg_color = backgroundColor)
-    #     DrawBackground.place(x = self.appMenuXCoord3, y = 0)
-
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.title("GTA V Ps3 Menu Designer")
+
+        self.title(" GTA V Ps3 Menu Designer ")
         self.geometry(app_window)
         self.resizable(False, False)
-        
-        self.safeZoneSizeX, self.safeZoneSizeY = GetSafeZoneSizeHalved()
-        self.appMenuXCoord1, self.appMenuXCoord2, self.appMenuXCoord3, self.leftSided = SetMenuPositon()
-        
-        # App background Image.     
-        self.app_draw_bg_image()
 
-        self.SafeZoneFrame = SafeZoneFrame(self)
-        self.SafeZoneFrame.place(x = 10, y = 10)
+        self.app_bg_image(self)
 
-    def app_draw_bg_image(self):
-        get_bg_image = DrawImage(bg_img_path, app_width, app_height)
-        app_bg_image = customtkinter.CTkLabel(self, image = get_bg_image, text = "")
-        app_bg_image.grid(row = 0, column = 0)
+    def app_bg_image(self):
+        img_path = '.\\img\\background.png'
+        width = app_width
+        height = app_height
+        self.app_background = DrawImage(img_path, width, height)
+        self.app_bg_image = customtkinter.CTkLabel(self, image = self.app_background, text = "")
+        self.app_bg_image.grid(row = 0, column = 0)
 
 if __name__ == '__main__':
-    app = App()
-    app.mainloop()
+    app = App().mainloop()
--- src/functions.py
+++ src/functions.py
@@ -1,54 +1,9 @@
-import customtkinter
-from PIL import Image
 from src.settings import *
 
-# Criando zona segura na tela do app, conforme no GTA.
-# GET_SAFE_ZONE_SIZE = 0x3BDC44 (em decimal = 3894012)
-# safezoneSizeX = app_width * 0.6 / 2
-# safezoneSizeY = app_height * 0.9 / 2
-# Defina a zona segura: Assim, a zona segura vai de (64, 36) até (1216, 684).
-def GetSafeZoneSizeHalved():
-    safeZoneSizeX = (app_width * 0.9) / 2
-    safeZoneSizeY = app_height * 0.9
-    return safeZoneSizeX, safeZoneSizeY
-
-def SetMenuPositon():
-    safeZoneSizeX, safeZoneSizeY = GetSafeZoneSizeHalved()
-    if leftSided == 1:
-        MenuXCoord1 = int(safeZoneSizeX * 0.5061)
-        MenuXCoord2 = int(safeZoneSizeX * 0.7211)
-        MenuXCoord3 = int(safeZoneSizeX * 0.6136)
-        # Converting
-        appMenuXCoord1 = MenuXCoord1 - safeZoneSizeX
-        appMenuXCoord2 = MenuXCoord2 - safeZoneSizeX
-        appMenuXCoord3 = MenuXCoord3 - safeZoneSizeX
-        return appMenuXCoord1, appMenuXCoord2, appMenuXCoord3, leftSided
-    else:
-        MenuXCoord1 = int(safeZoneSizeX * 0.2789)
-        MenuXCoord2 = int(safeZoneSizeX * 0.4939)
-        MenuXCoord3 = int(safeZoneSizeX * 0.3864)
-        # Converting
-        appMenuXCoord1 = MenuXCoord1 + safeZoneSizeX
-        appMenuXCoord2 = MenuXCoord2 + safeZoneSizeX
-        appMenuXCoord3 = MenuXCoord3 + safeZoneSizeX
-        return appMenuXCoord1, appMenuXCoord2, appMenuXCoord3, leftSided
-
-# Resource for drawing images.
-def DrawImage(path, width, height):
-    set_image = customtkinter.CTkImage(
-        dark_image = Image.open(path),
-        size = (width, height)
-    )
-    return set_image
-
-# Resource for drawing rects.
-class DrawRect(customtkinter.CTkFrame):
-    def __init__(self, master, width, height, fg_color):
-        width = int(app_width * width)
-        height = int(app_height * height)
-        super().__init__(master, width = width, height = height, fg_color = fg_color, corner_radius = 0)
-        # if MenuBGImage == '1':
-            # self.image_path = bg_custom_img_path
-            # self.GetBGImage = DrawImage(self.image_path, width = width, height = height)
-            # self.DrawBGImage = customtkinter.CTkLabel(self, image = self.GetBGImage, text = "")
-            # self.DrawBGImage.grid(row = 0, column = 0)
+class DrawImage():
+    def __init__(img_path, width, height):
+        set_draw_image = customtkinter.CTkImage(
+            dark_image = Image.open(img_path),
+            size = (width, height)
+        )
+        return set_draw_image
